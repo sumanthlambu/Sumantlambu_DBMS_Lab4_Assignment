028e40b401872b6939eca726f97fab019a0d95e8
sumanthlambu <119809337+sumanthlambu@users.noreply.github.com>
2023-02-28 14:19:06 +0530

    Add files via upload

---
+++ 01_Create Tables/00_Drop_tables.sql
@@ -0,0 +1,9 @@
+use ecommerce;
+drop table IF EXISTS rating;
+drop table IF EXISTS `order`;
+drop table IF EXISTS product_details;
+drop table IF EXISTS supplier_pricing;
+drop table IF EXISTS supplier;
+drop table IF EXISTS product;
+drop table IF EXISTS category;
+drop table IF EXISTS customer;
